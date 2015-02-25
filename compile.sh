5bc90c09cc57501d977bd501bd9b1f34b562a8c4
Caagr98 <Caagr98@users.noreply.github.com>
2015-02-25 18:25:55 +0100

    Bugfixes

--- compile.sh
+++ compile.sh
@@ -1,1 +1,4 @@
-cat <(echo -e "/*** This stylesheet is automatically generated ***/") <(sass main.scss --style compact) > style.css
+cat \
+  <(echo -e "/*** This stylesheet is automatically generated, the source can be found at https://github.com/Caagr98/C98Mods-style ***/") \
+  <(sass main.scss --style compact) \
+ > style.css
